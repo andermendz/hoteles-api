e931ee3ea2c0c079ed7cda38f7785fc1adfe6746
andermendz <andersonmendz19@gmail.com>
2024-12-04 21:06:30 -0500

    modelos con relacioens y  migraciones

---
+++ app/Models/Hotel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    protected $fillable = ['name', 'address', 'city', 'nit', 'total_rooms'];
+
+    public function hotelRooms()
+    {
+        return $this->hasMany(HotelRoom::class);
+    }
+}
