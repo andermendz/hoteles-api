85d69ef99eddb4860c35afc013653bb80945f6d3
andermendz <andersonmendz19@gmail.com>
2024-12-04 23:19:53 -0500

    api changes

--- app/Http/Controllers/RoomTypeController.php
+++ app/Http/Controllers/RoomTypeController.php
@@ -1,16 +1,25 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\RoomType;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class RoomTypeController extends Controller
 {
-    public function index(): JsonResponse
+    public function index()
     {
-        $roomTypes = RoomType::all();
-        return response()->json($roomTypes);
+        return RoomType::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:room_types'
+        ]);
+
+        $roomType = RoomType::create($validated);
+        return response()->json($roomType, 201);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HotelController;
+use App\Http\Controllers\RoomTypeController;
+use App\Http\Controllers\AccommodationController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/hotels', [HotelController::class, 'index']);
+Route::post('/hotels', [HotelController::class, 'store']);
+Route::get('/room-types', [RoomTypeController::class, 'index']);
+Route::get('/accommodations', [AccommodationController::class, 'index']);
